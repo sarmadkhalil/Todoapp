eb9c66286ae5d4ef199eb23c6d883bf55a192f6e
Sarmad khalil <sarmadkhalil97@gmail.com>
2019-09-24 01:35:46 +0800

    Signed-off-by: Sarmad khalil <sarmadkhalil97@gmail.com>

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -21,7 +21,7 @@
                   {{ $todo->name }}
                   <a href="/todos/{{ $todo->id }}" class="btn btn-primary btn-sm float-right">View</a>
                   <a href="todos/{{$todo->id}}/edit" class="btn btn-warning btn-sm float-right mx-1">Edit</a>
-                  <a href="/todos/{{ $todo->id }}/delete" class="btn btn-success btn-sm float-right">Complete</a>
+                  <a href="/todos/{{ $todo->id }}/delete" class="btn btn-success btn-sm float-right">Completed</a>
                 </li>
               @endforeach
             </ul>
